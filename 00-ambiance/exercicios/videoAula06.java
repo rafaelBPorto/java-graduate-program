54254d06447f65cd5a4cbb7f32a32f8558e10c52
Rafael Porto <rafaelbporto88@gmail.com>
2026-05-04 11:18:28 -0300

    finalizazao exercicio videoaula06

--- 00-ambiance/exercicios/videoAula06.java
+++ 00-ambiance/exercicios/videoAula06.java
@@ -9,19 +9,30 @@
       que retorna o valor total dos itens em estoque (preço * quantidade). 
   - Crie uma instância de Produto no método main e exiba o valor total em estoque.
   
-  Exercício 2: Na classe Produto do exercício anterior, adicione um método
-  void adicionarEstoque(int quantidade) que aumenta a quantidade de
-  itens em estoque. Adicione também um método boolean
-  removerEstoque(int quantidade) que diminui a quantidade em estoque
-  e retorna true se a operação foi bem-sucedida (havia estoque suficiente)
-  ou false caso contrário (a quantidade a ser removida é maior que o
-  estoque atual). Teste os dois métodos no main.
+  Exercício 2: Na classe Produto do exercício anterior, 
+  - adicione um método:
+     void adicionarEstoque(int quantidade) que aumenta a quantidade de itens em estoque. 
+  - Adicione também um método boolean removerEstoque(int quantidade) 
+    que diminui a quantidade em estoque e retorna true se a operação foi bem-sucedida (havia estoque suficiente)
+    ou false caso contrário (a quantidade a ser removida é maior que o estoque atual). 
+  - Teste os dois métodos no main.
 */
 
 void main(){
  Produto pera = new Produto(1, "pera", 1.99, 10);
 
- IO.println("Valor total em " + pera.nome)
+ IO.println("Valor total em " + pera.nome + " é: " + pera.valorTotalEmEstoque());
+ IO.println("Total em estoque: " + pera.quantidadeEmEstoque);
+ pera.adicionarEstoque(10);
+ IO.println("Total em estoque: " + pera.quantidadeEmEstoque);
+
+ IO.println(pera.removerEstoque(10));
+ IO.println("Total em estoque: " + pera.quantidadeEmEstoque);
+
+ IO.println(pera.removerEstoque(11));
+ IO.println("Total em estoque: " + pera.quantidadeEmEstoque);
+ 
+
 }
 
 class Produto {
@@ -38,11 +49,23 @@ class Produto {
     this.codigo = codigo;
     this.nome = nome;
     this.preco = preco;
-    this.quantidadeEmEstoque = quantidadeEmEstoque
+    this.quantidadeEmEstoque = quantidadeEmEstoque;
   }
 
   //Metodos
   double valorTotalEmEstoque(){
     return quantidadeEmEstoque * preco;
+  }
+
+  void adicionarEstoque(int qtd) {
+    this.quantidadeEmEstoque += qtd;
+  }
+
+  boolean removerEstoque(int quantidade){
+    if (this.quantidadeEmEstoque < quantidade) return false;
+
+    this.quantidadeEmEstoque -= quantidade;
+
+    return true;
   }
 }
